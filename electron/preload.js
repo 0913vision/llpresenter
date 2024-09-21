e9bb55dbb1ddb7151135fe57f7fb65a595b008a3
0913vision <0913vision@gmail.com>
2024-09-22 05:28:30 +0900

    Refactor electron/preload.js to use a custom menu in setComponentMenu

--- electron/preload.js
+++ electron/preload.js
@@ -5,5 +5,5 @@ contextBridge.exposeInMainWorld('electronAPI', {
   handleFileUpload: callback => ipcRenderer.on('lyrics_file_upload', callback),
   // disableMenuShortcuts: () => ipcRenderer.send('disable-menu-shortcuts'),
   // enableMenuShortcuts: () => ipcRenderer.send('enable-menu-shortcuts'),
-  setComponentMenu: (menuName) => ipcRenderer.send('set-component-menu', menuName),
+  setComponentMenu: (customMenu) => ipcRenderer.send('set-component-menu', customMenu),
 });
